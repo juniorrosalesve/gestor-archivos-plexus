f2957469d3bf1f085fd35592fc8b0b6e402f5336
Júnior Rosales <74562615+juniorrosalesve@users.noreply.github.com>
2023-09-04 13:52:09 -0500

    fix

--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -15,71 +15,71 @@ class RegionSeeder extends Seeder
      */
     public function run(): void
     {
-        $region1    =   [
-            'México',
-            'Puerto Rico',
-            'Chile'
-        ];
-        Region::create([
-            'name' => 'Región 1',
-            'userId' => 2
-        ]);
+        // $region1    =   [
+        //     'México',
+        //     'Puerto Rico',
+        //     'Chile'
+        // ];
+        // Region::create([
+        //     'name' => 'Región 1',
+        //     'userId' => 2
+        // ]);
 
-        $region2    =   [
-            'Colombia',
-            'República Dominicana',
-            'Ecuador'
-        ];
-        Region::create([
-            'name' => 'Región 2',
-            'userId' => 3
-        ]);
+        // $region2    =   [
+        //     'Colombia',
+        //     'República Dominicana',
+        //     'Ecuador'
+        // ];
+        // Region::create([
+        //     'name' => 'Región 2',
+        //     'userId' => 3
+        // ]);
 
-        $region3    =   [
-            'Perú',
-            'Paraguay',
-            'Bolivia'
-        ];
-        Region::create([
-            'name' => 'Región 3',
-            'userId' => 4
-        ]);
+        // $region3    =   [
+        //     'Perú',
+        //     'Paraguay',
+        //     'Bolivia'
+        // ];
+        // Region::create([
+        //     'name' => 'Región 3',
+        //     'userId' => 4
+        // ]);
 
-        $region4    =   [
-            'Costa Rica',
-            'Guatemala',
-            'Honduras',
-            'El Salvador',
-            'Panamá'
-        ];
-        Region::create([
-            'name' => 'Región 4',
-            'userId' => 5
-        ]);
+        // $region4    =   [
+        //     'Costa Rica',
+        //     'Guatemala',
+        //     'Honduras',
+        //     'El Salvador',
+        //     'Panamá'
+        // ];
+        // Region::create([
+        //     'name' => 'Región 4',
+        //     'userId' => 5
+        // ]);
 
-        foreach($region1 as $item) {
-            Country::create([
-                'name' => $item,
-                'regionId' => 1
-            ]);
-        }
-        foreach($region2 as $item) {
-            Country::create([
-                'name' => $item,
-                'regionId' => 2
-            ]);
-        }
-        foreach($region3 as $item) {
-            Country::create([
-                'name' => $item,
-                'regionId' => 3
-            ]);
-        }
-        foreach($region4 as $item) {
-            Country::create([
-                'name' => $item,
-                'regionId' => 4
-            ]);
-        }
+        // foreach($region1 as $item) {
+        //     Country::create([
+        //         'name' => $item,
+        //         'regionId' => 1
+        //     ]);
+        // }
+        // foreach($region2 as $item) {
+        //     Country::create([
+        //         'name' => $item,
+        //         'regionId' => 2
+        //     ]);
+        // }
+        // foreach($region3 as $item) {
+        //     Country::create([
+        //         'name' => $item,
+        //         'regionId' => 3
+        //     ]);
+        // }
+        // foreach($region4 as $item) {
+        //     Country::create([
+        //         'name' => $item,
+        //         'regionId' => 4
+        //     ]);
+        // }
     }
 }
